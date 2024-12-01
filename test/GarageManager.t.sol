c93868ddc8ab5c1f882918930cdd20b9d23f5696
VisualReszka <contact@visualreszka.art>
2024-12-01 21:08:13 +0000

    Clean up repository: keep only GarageManager.sol and README.md

--- test/GarageManager.t.sol
+++ test/GarageManager.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "forge-std/Test.sol";
+import "lib/forge-std/src/Test.sol";
 import "../src/GarageManager.sol";
 
 contract GarageManagerTest is Test {
